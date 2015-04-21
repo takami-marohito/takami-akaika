2354814551710eff188c1885ac14bdd11168fa8b
takami-marohito <takami@viz.media.kyoto-u.ac.jp>
2015-04-21 12:20:34 +0900

    add sample code loading the file

--- js/FormControl.js
+++ js/FormControl.js
@@ -20,6 +20,13 @@ function SelectVariableEvent()
             if(target2.value == "LoadingCalculatedVariable"){
                 document.getElementById("SecondInvariant").style.display="none";
                 document.getElementById("Filename").style.display="";
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    var data = d3.csv.parseRows(e.target.result);
+                    console.log(data);
+                };
+                reader.readAsText(document.getElementById("Filename").files[0],'UTF-8');
+                //console.log(reader.result);
             }
         });
     });
