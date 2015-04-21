c16d17e62a5e944eb02529a99d4c66345f4a4f17
takami-marohito <takami@viz.media.kyoto-u.ac.jp>
2015-04-21 10:59:50 +0900

    add file select button, but don't work yet

--- js/FormControl.js
+++ js/FormControl.js
@@ -2,17 +2,24 @@
  * Created by vizlab on 2015/04/09.
  */
 
-function SelectVariable()
+
+function SelectVariableEvent()
 {
     jQuery(function() {
         jQuery('#VariableMode').on('change', function(){
             var target2 = document.getElementById("VariableMode");
             console.log(target2.value + " selected");
             if(target2.value == "SecondInvariant"){
-                document.getElementById("SecondInvariant").style.display="inline";
+                document.getElementById("SecondInvariant").style.display="";
+                document.getElementById("Filename").style.display="none";
             }
             if(target2.value == "VortexRotation"){
                 document.getElementById("SecondInvariant").style.display="none";
+                document.getElementById("Filename").style.display="none";
+            }
+            if(target2.value == "LoadingCalculatedVariable"){
+                document.getElementById("SecondInvariant").style.display="none";
+                document.getElementById("Filename").style.display="";
             }
         });
     });
